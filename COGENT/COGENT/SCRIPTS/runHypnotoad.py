a5ef68c81d6e8548ab6afadd39d4818e85cd0883
Charles Reynolds <reynolds12@llnl.gov>
2015-04-20 11:49:45 -0700

    saved latest changes before giving to Milo to explore

--- COGENT/COGENT/SCRIPTS/runHypnotoad.py
+++ COGENT/COGENT/SCRIPTS/runHypnotoad.py
@@ -5,19 +5,38 @@ import os.path
 
 print ('running runHypnoToad.py (file:"%s")' % __file__)
 
-idl_files_dir = str(root['SETTINGS']['SETUP']['hypnotoad_location'])
+# Copied elswhere - need to import somehow:
+import shutil
+
+def copy_to_other_working(work_path, other_work_dir):
+    """other_work_dir can be a complete file name or just a directory,
+    just like unix cp.
+    """
+    shutil.copy(work_path, other_work_dir) 
+
+def move_from_other_working(work_path, other_work_dir):
+    """other_work_dir can only be a direcotory.
+    """
+    work_dir = os.path.dirname(work_path)
+    name = os.path.basename (work_path)
+    other_work_path = other_work_dir + os.sep + name
+    # Have to use the complete work path as destination because every 
+    # working OMFIT file is in its own directory:
+    shutil.move(other_work_path, work_path) 
+# END Copied elswhere - need to import somehow.
+
+copy_to_other_working(root['FILES']['G-EQDSK'], idl_work_dir)
+
 idl_statement = 'hypnotoad'
+arguments='-e ' + idl_statement,
+# useful if all IDL licenses are taken.  Runs for 7 minutes:
+# arguments='-demo ' + arguments,
 
-geqdsk_path = str(root['FILES']['G-EQDSK'])
-idl_statement += ', GEQDSK_DIR="' + os.path.dirname(geqdsk_path)+ '"'
-idl_statement += ', GEQDSK_FILE="' + os.path.basename(geqdsk_path) + '"'
-
-grid_path = str(root['FILES']['grid'])
-idl_statement += ', GRID_DIR="' + os.path.dirname(grid_path) + '"'
-idl_statement += ', GRID_FILE="' + os.path.basename(grid_path) + '"'
+# For now, the IDL source dir is also the working dir:
+idl_source_dir = idl_work_dir
 
 OMFITx.execute(
-    command_line='cd ' + idl_files_dir + ';' +  
+    command_line='cd ' + idl_source_dir + ';' +  
                  'module add idl;' + 
                  'idl',
     interactive_input=None,
@@ -25,8 +44,11 @@ OMFITx.execute(
     std_out=None,
     std_err=None,
     quiet=False,
-    arguments='-e ' + "'" + idl_statement + "'",
-#    arguments='-demo -e ' + idl_statement,
+    arguments=arguments,
     script=None)
+
+# Have to copy each file separately because every 
+# working OMFIT file lives in its own directory:
+move_from_other_working(root['FILES']['hypnotoad_state'], idl_work_dir)
 
 print ('DONE running runHypnoToad.py')
--- COGENT/COGENT/SCRIPTS/run_cogent_grid.py
+++ COGENT/COGENT/SCRIPTS/run_cogent_grid.py
@@ -5,15 +5,40 @@ import os.path
 
 print ('running run_cogent_grid.py (file:"%s")' % __file__)
 
-idl_files_dir = str(root['SCRIPTS']['cogent_grid location'])
-idl_statement = 'cogent_grid'
+# Same as runHypntoad.py:
+import shutil
 
-#grid_path = str(root['FILES']['grid'])
-#idl_statement += ', GRID_DIR="' + os.path.dirname(grid_path) + '"'
-#idl_statement += ', GRID_FILE="' + os.path.basename(grid_path) + '"'
+def copy_to_other_working(work_path, other_work_dir):
+    """other_work_dir can be a complete file name or just a directory,
+    just like unix cp.
+    """
+    shutil.copy(work_path, other_work_dir) 
+
+def move_from_other_working(work_path, other_work_dir):
+    """other_work_dir can only be a direcotory.
+    """
+    work_dir = os.path.dirname(work_path)
+    name = os.path.basename (work_path)
+    other_work_path = other_work_dir + os.sep + name
+    # Have to use the complete work path as destination because every 
+    # working OMFIT file is in its own directory:
+    shutil.move(other_work_path, work_path) 
+# END Same as runHypntoad.py.
+
+idl_work_dir = str(root['SETTINGS']['HYPNOTOAD']['workDir'])
+
+copy_to_other_working(root['FILES']['hypnotoad_state'], idl_work_dir)
+
+idl_statement = 'cogent_grid'
+arguments='-e ' + idl_statement,
+# useful if all IDL licenses are taken.  Runs for 7 minutes:
+# arguments='-demo ' + arguments,
+
+# For now, the IDL source dir is also the working dir:
+idl_source_dir = idl_work_dir
 
 OMFITx.execute(
-    command_line='cd ' + idl_files_dir + ';' +  
+    command_line='cd ' + idl_source_dir + ';' +  
                  'module add idl;' + 
                  'idl',
     interactive_input=None,
@@ -21,8 +46,14 @@ OMFITx.execute(
     std_out=None,
     std_err=None,
     quiet=False,
-    arguments='-e ' + "'" + idl_statement + "'",
-#    arguments='-demo -e ' + idl_statement,
+    arguments=arguments,
     script=None)
+
+# Have to copy each file separately because every 
+# working OMFIT file lives in its own directory:
+move_from_other_working(root['FILES']['hypnotoad_state'], idl_work_dir) # really only need delete here
+move_from_other_working(root['FILES']['nodal_COGENT_Coefficients'], idl_work_dir)
+move_from_other_working(root['FILES']['nodal_grid'], idl_work_dir)
+
 
 print ('DONE running run_cogent_grid.py')
