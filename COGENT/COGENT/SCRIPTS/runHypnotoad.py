cfcb56ee5e6317232908e5263ba23ee251b0a9d6
Charles Reynolds <reynolds12@llnl.gov>
2015-05-01 11:06:52 -0700

    fixed file copy to and from Hypnotoad dir

--- COGENT/COGENT/SCRIPTS/runHypnotoad.py
+++ COGENT/COGENT/SCRIPTS/runHypnotoad.py
@@ -1,30 +1,42 @@
 # Created by creyn at 2015/02/13 10:35
 
-import os.path
-
 print ('running runHypnoToad.py (file:"%s")' % __file__)
 
 # Copied to other script(s) - need to import somehow:
+import os
+import os.path
 import shutil
 
 def copy_to_other_work_dir(work_path, other_work_dir):
+    work_path = str(work_path)
+    other_work_dir = str(other_work_dir)
     assert os.path.isdir(other_work_dir), other_work_dir
     shutil.copy(work_path, other_work_dir) 
 
 def move_from_other_work_dir(work_path, other_work_dir):
+    work_path = str(work_path)
+    other_work_dir = str(other_work_dir)
     assert os.path.isdir(other_work_dir), other_work_dir
     file_name = os.path.basename(work_path)
     other_work_path = os.path.join(other_work_dir, file_name)
     # Have to use the complete work path as destination because every 
     # working OMFIT file is in its own directory:
     shutil.move(other_work_path, work_path) 
+
+def remove_from_other_work_dir(work_path, other_work_dir):
+    work_path = str(work_path)
+    other_work_dir = str(other_work_dir)
+    assert os.path.isdir(other_work_dir), other_work_dir
+    file_name = os.path.basename(work_path)
+    other_work_path = os.path.join(other_work_dir, file_name)
+    os.remove(other_work_path) 
 # END Copied to other script(s) - need to import somehow.
 
 idl_work_dir = root['SETTINGS']['HYPNOTOAD']['workDir']
 copy_to_other_work_dir(root['FILES']['G-EQDSK'], idl_work_dir)
 
 idl_statement = 'hypnotoad'
-arguments='-e ' + idl_statement,
+arguments=' -e ' + idl_statement,
 # useful if all IDL licenses are taken.  Runs for 7 minutes:
 # arguments='-demo ' + arguments,
 idl_source_dir = root['SETTINGS']['HYPNOTOAD']['sourceDir']
@@ -43,5 +55,7 @@ OMFITx.execute(
 # Have to do each file separately because every working OMFIT file lives in 
 # its own directory:
 move_from_other_work_dir(root['FILES']['hypnotoad_state'], idl_work_dir)
+move_from_other_work_dir(root['FILES']['COGENT_Coefficients'], idl_work_dir)
+remove_from_other_work_dir(root['FILES']['G-EQDSK'], idl_work_dir)
 
 print ('DONE running runHypnoToad.py')
